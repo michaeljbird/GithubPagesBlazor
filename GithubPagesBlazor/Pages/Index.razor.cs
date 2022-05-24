c44880a18f08764a355b2e6398111b2c1477d21f
michaeljbird <michael.bird.web@gmail.com>
2022-05-24 21:34:11 +0200

    Initial implementation of localization

---
+++ GithubPagesBlazor/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace GithubPagesBlazor.Pages
+{
+    public partial class Index
+    {
+
+        [Inject] IStringLocalizerFactory _localizerFactory { get; set; }
+
+        private IStringLocalizer _localizer => _localizerFactory.Create(typeof(Index));
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+
+    }
+}
